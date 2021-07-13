4344587b68e685a654988b27725bc990a5500588
JohnE-ZNB <johne.cr@hotmail.com>
2021-07-12 19:19:58 -0500

    Commit de la clase 11 Optimizar fuentes

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,16 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
-/* 
-    Entry: Nombre desde donde inicia el proyecto
-    output:
-      path= Nombre del directorio - path.resolve evita tener problemas con el servidor por la ubicacion
-      filename: Nombre del archivo resultante de webpack al compilar
-    resolve: las extensiones que se debe tener en cuenta para compilar y unificar
-*/
+
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.js", //Entry: Nombre desde donde inicia el proyecto
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    path: path.resolve(__dirname, "dist"), //path= Nombre del directorio - path.resolve evita tener problemas con el servidor por la ubicacion
+    filename: "main.js", // filename: Nombre del archivo resultante de webpack al compilar
+    assetModuleFilename: "assets/images/[hash][ext][query]", //La Ruta hacia donde moveremos los assets
   },
   resolve: {
-    extensions: [".js"],
+    extensions: [".js"], //resolve: las extensiones que se debe tener en cuenta para compilar y unificar
   },
   module: {
     //Son la reglas (rules) que vamos aplicar para babel-loader SIEMPRE en EXPRESIONES REGULARES
@@ -42,6 +37,21 @@ module.exports = {
         test: /\.png/,
         // Usaremos el resource que viene ya por defecto con webpack
         type: "asset/resource",
+      },
+      {
+        // Test declara que extensión de archivos que aplicara para el loader
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff", // Habilita o deshabilita la transformación de archivos en base64.
+            name: "[name].[ext]", //Para que respete el nombre y extensión original
+            outputPath: "./assets/fonts/", //Donde va a quedar el archivo
+            publicPath: "./assets/fonts/", //Donde queda publicamente
+            esModule: false,
+          },
+        },
       },
     ],
   },
